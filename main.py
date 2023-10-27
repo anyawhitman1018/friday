05f8f999fe2da9fe68453c56eba895671770bc53
anyawhitman1018 <annawhitman@ufl.edu>
2023-10-27 08:13:12 -0400

    This is the first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+string = "I am happy that it is friday"
+
+#This is my first for loop
+for i in range(0, len(string), 4):
+    print(string[i], end = ":")
+
+#I am going to commit the first loop now
+
