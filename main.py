6849698699d1e6758829b3c604771cc0f55931c8
anyawhitman1018 <annawhitman@ufl.edu>
2023-10-27 08:16:00 -0400

    This is my second commit

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ for i in range(0, len(string), 4):
     print(string[i], end = ":")
 
 #I am going to commit the first loop now
-
+def making_a_list():
+    for i in range(0, len(string), 4):
+        output = []
+        output.append(string[i])
+    return output 
